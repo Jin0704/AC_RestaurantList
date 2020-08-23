d536829af98f719f00d3b92bfae54454dd319e38
Jin <s87160204@gmail.com>
2020-08-23 14:51:58 +0800

    Fix index and app.js by each

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 
@@ -10,9 +12,12 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
 })
 
+app.get('/restaurants/:1', (req, res) => {
+  res.render('show')
+})
 
 app.listen(port, () => {
   console.log(`HW restaurantList on http://localhost:${port}`)
