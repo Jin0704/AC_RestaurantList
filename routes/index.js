c6307bfe99bb71d40af99aa3065a9c4b2f07e69e
Jin <s87160204@gmail.com>
2020-09-13 23:46:35 +0800

    Fix index.js

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,9 @@ router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/search', restaurants)
 router.use("/sort", sort)
+router.use('/sort/name/restaurants', restaurants)
+router.use('/sort/category/restaurants', restaurants)
+router.use('/sort/location/restaurants', restaurants)
+router.use('/sort/rating/restaurants', restaurants)
 
 module.exports = router
