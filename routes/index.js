09c48d1d50e80d60600daef2d5d35f7fe689c1c2
Jin <s87160204@gmail.com>
2020-11-08 23:00:31 +0800

    feat:add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const sort = require("./modules/sort")
-
+const users = require('./user')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
@@ -14,5 +14,6 @@ router.use('/sort/name/restaurants', restaurants)
 router.use('/sort/category/restaurants', restaurants)
 router.use('/sort/location/restaurants', restaurants)
 router.use('/sort/rating/restaurants', restaurants)
+router.use('/users', users)
 
 module.exports = router
