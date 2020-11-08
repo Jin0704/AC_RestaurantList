199b9cb5bf2ef76629daa1bdc363fa6ecf9f5cab
Jin <s87160204@gmail.com>
2020-11-08 23:09:04 +0800

    feat:add user login page

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const { model } = require('../models/restaurant')
 const router = express.Router()
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
 
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 
 module.exports = router
