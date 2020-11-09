e589c4652a4c76630a254cf3d84ab2659373fa24
Jin <s87160204@gmail.com>
2020-11-09 23:10:54 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const router = express.Router()
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
 const PORT = process.env.PORT || 3000
 const exphbs = require('express-handlebars')
 require('./config/mongoose')
@@ -28,11 +29,13 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   console.log(req.user)
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 app.use(routes) //首頁路由
